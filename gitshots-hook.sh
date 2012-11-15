59f1e8142e0968109fac8cca8d13ee3e9a36fe75
Ryan Regalado <ryan@design48.net>
2012-11-14 23:43:32 -0800

    add file to buffer for gitshots

--- gitshots-hook.sh
+++ gitshots-hook.sh
@@ -3,5 +3,6 @@ file="~/.gitshots/#{Time.now.to_i}.jpg"
 unless File.directory?(File.expand_path("../../rebase-merge", __FILE__))
   puts "Taking capture into #{file}!"
   system "imagesnap -q -w 3 #{file} &"
+  exec "echo #{file} | pbcopy"
 end
 exit 0
