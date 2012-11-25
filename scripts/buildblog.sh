1707dbd3b7d105422bd50a2c602155d6274db64e
Ryan Regalado <ryan@design48.net>
2012-11-24 23:47:24 -0800

    scripts for jekyll blog and mytestimony dev tooling

---
+++ scripts/buildblog.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+# This script copies all generated _site files from a jekyll bootstrap blog,
+# commits to a git repo, and pushes it up to github to be publicly viewable
+
+echo "Jumping to design48.github.com repo"
+cd ~/Dropbox/Dev/design48.github.com
+
+echo "Copying _site/tags to main repo directory"
+cp -r _site/tags .
+
+echo "Commiting to repo"
+git add .
+git commit -m "latest build of site"
+
+echo "Commiting to repo"
+git push
+
+echo "Complete"
+exit
