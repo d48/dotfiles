3d779458872c5705b6bb818d041a1cf7bdadf747
Ryan Regalado <ryan@design48.net>
2013-01-09 21:25:15 -0800

    propery get pid

--- scripts/stop-launch.sh
+++ scripts/stop-launch.sh
@@ -2,7 +2,7 @@
 
 # prints first column from output of jobs
 # todo: need better grep/search for launch-app.sh
-JOBLINE=`ps ax | grep [scripts/launch]-app.sh`
+JOBLINE=`ps ax | grep [scripts/launch]-app.sh | awk '{print $1}'`
 
 echo "running kill -TERM -${JOBLINE}"
 kill -TERM -${JOBLINE}
